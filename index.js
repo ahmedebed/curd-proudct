70db91944c2b8e4a5f23bd30d3249e4c641c844a
ahmedebed <ahmedebed69@gmail.com>
2025-03-08 21:43:58 +0200

    add delete update proudct"

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ let total = document.getElementById("total");
 let count = document.getElementById("count");
 let category = document.getElementById("category");
 let create = document.getElementById("create");
-let search = document.getElementById("search");
+//let search = document.getElementById("search");
 let searchTitle = document.getElementById("searchTitle");
 let searchCategory = document.getElementById("searchCategory");
+
+let mood = "create";
+let tmp;
 // get Total
 function getTotal() {
   if (price.value != "") {
@@ -24,7 +27,7 @@ function getTotal() {
 //create proudct
 
 //array 34an a7ot feha data
-let dataPro;
+let dataPro = [];
 if (localStorage.product != null) {
   dataPro = JSON.parse(localStorage.product);
 } else {
@@ -42,7 +45,22 @@ create.onclick = function () {
     count: count.value,
     category: category.value,
   };
-  dataPro.push(newPro);
+
+  if (mood === "create") {
+    if (newPro.count > 1) {
+      for (let i = 0; i < newPro.count; i++) {
+        dataPro.push(newPro);
+      }
+    } else {
+      dataPro.push(newPro);
+    }
+  } else {
+    dataPro[tmp] = newPro;
+    mood = "create";
+    count.style.display = "block";
+    create.innerHTML = "Create";
+  }
+
   localStorage.setItem("product", JSON.stringify(dataPro));
   clearDate();
   showData();
@@ -70,10 +88,58 @@ function showData() {
     <td>${dataPro[i].discount}</td>
     <td>${dataPro[i].total}</td>
     <td>${dataPro[i].category}</td>
-    <td><button id="update">Update</button></td>
-    <td><button id="delete">Delete</button></td>
+    <td><button id="update" onclick="updataData(${i})">Update</button></td>
+    <td><button onclick="deleteProudct(${i})" id="delete">Delete</button></td>
   </tr>`;
   }
+
   document.getElementById("tbody").innerHTML = table;
+  let btnDeleteAll = document.getElementById("deleteAll");
+  if (dataPro.length > 0) {
+    btnDeleteAll.innerHTML = `<button onclick="deleteAll()">Delete All(${dataPro.length})</button>`;
+  } else {
+    btnDeleteAll.innerHTML = ``;
+  }
+  getTotal();
 }
 showData();
+//delete item
+function deleteProudct(i) {
+  dataPro.splice(i, 1);
+  localStorage.product = JSON.stringify(dataPro);
+  showData();
+}
+//delete All Item
+function deleteAll() {
+  localStorage.clear();
+  dataPro.splice(0);
+  showData();
+}
+//updateData
+function updataData(i) {
+  tmp = i;
+  title.value = dataPro[i].title;
+  price.value = dataPro[i].price;
+  taxs.value = dataPro[i].taxs;
+  ads.value = dataPro[i].ads;
+  discount.value = dataPro[i].discount;
+  getTotal();
+  count.style.display = "none";
+  category.value = dataPro[i].category;
+  create.innerHTML = "Update";
+  mood = "update";
+  scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+//search
+let searchMood = "title";
+function getSearchMood(id) {
+  console.log(id);
+  if (id == "searchTitle") {
+    searchMood = "title";
+  } else {
+    searchMood = "category";
+  }
+}
